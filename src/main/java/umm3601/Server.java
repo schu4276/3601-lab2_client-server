5dadfb98c32dc746742279d3c5208ddc73cfb8e2
Nic McPhee <mcphee@morris.umn.edu>
2017-09-11 19:39:25 -0500

    Improve the error handling
    This checks that the `UserController` is properly constructed, and shuts
    the server down semi-gracefully if it isn't.
    
    It also checks that if we search for a `User` and don't find it we
    return a responable response that tells the client that we didn't find
    it.

--- src/main/java/umm3601/Server.java
+++ src/main/java/umm3601/Server.java
@@ -3,6 +3,9 @@ package umm3601;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import spark.Request;
+import spark.Response;
+import umm3601.user.User;
 import umm3601.user.UserController;
 
 import java.io.IOException;
@@ -10,42 +13,82 @@ import java.io.IOException;
 import static spark.Spark.*;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
-        staticFiles.location("/public");
-        Gson gson = new Gson();
-        UserController userController = new UserController();
 
-        // Simple example route
-        get("/hello", (req, res) -> "Hello World");
+  public static final String USER_DATA_FILE = "src/main/data/users.json";
 
-        // Redirects for the "home" page
-        redirect.get("", "/");
-        redirect.get("/", "/index.html");
+  public static void main(String[] args) {
+    Server server = new Server();
 
-        // Redirect for the "about" page
-        redirect.get("/about", "/about.html");
+    staticFiles.location("/public");
 
-        // Redirect for the Users Form
-        redirect.get("/users", "/users.html");
+    Gson gson = new Gson();
 
-        // List users
-        get("api/users", (req, res) -> {
-            res.type("application/json");
-            return wrapInJson("users", gson.toJsonTree(userController.listUsers(req.queryMap().toMap())));
-        });
+    final UserController userController = buildUserController();
 
-        // See specific user
-        get("api/users/:id", (req, res) -> {
-            res.type("application/json");
-            String id = req.params("id");
-            return gson.toJson(userController.getUser(id));
-        });
+    // Simple example route
+    get("/hello", (req, res) -> "Hello World");
+
+    // Redirects for the "home" page
+    redirect.get("", "/");
+    redirect.get("/", "/index.html");
+
+    // Redirect for the "about" page
+    redirect.get("/about", "/about.html");
+
+    // Redirect for the Users Form
+    redirect.get("/users", "/users.html");
+
+    // List users
+    get("api/users", (req, res) -> {
+      res.type("application/json");
+      return wrapInJson("users", gson.toJsonTree(userController.listUsers(req.queryMap().toMap())));
+    });
+
+    // See specific user
+    get("api/users/:id", (req, res) -> {
+      return getUser(gson, userController, req, res);
+    });
+  }
+
+  private static Object getUser(Gson gson, UserController userController, Request req, Response res) {
+    res.type("application/json");
+    String id = req.params("id");
+    User user = userController.getUser(id);
+    if (user != null) {
+      return gson.toJson(userController.getUser(id));
+    } else {
+      res.type("application/json");
+      return "{\"message\":\"User not found: User with ID " + id + " wasn't found.\"}";
+    }
+  }
+
+  /*
+   * Constructing the controller might throw an IOException if
+   * there are problems reading from the JSON "database" file.
+   * If that happens we'll print out an error message and shut
+   * the server down.
+   */
+  private static UserController buildUserController() {
+    UserController userController = null;
+
+    try {
+      userController = new UserController(USER_DATA_FILE);
+    } catch (IOException e) {
+      System.err.println("The server failed to load the user data; shutting down.");
+      e.printStackTrace(System.err);
+
+      // Shut the server down
+      stop();
+      System.exit(1);
     }
 
-    public static JsonObject wrapInJson(String name, JsonElement jsonElement) {
-        JsonObject result = new JsonObject();
-        result.add(name, jsonElement);
-        return result;
-    }
+    return userController;
+  }
+
+  private static JsonObject wrapInJson(String name, JsonElement jsonElement) {
+    JsonObject result = new JsonObject();
+    result.add(name, jsonElement);
+    return result;
+  }
 
 }
