5bd965cf6f32bc62d073f432f2a757c65542db26
Nic McPhee <mcphee@morris.umn.edu>
2017-09-12 00:22:54 -0500

    Remove commented out tests
    I accidentally left in some commented out tests I should have deleted.
    This fixes that.

--- src/test/java/umm3601/user/GetUserByIDFromDB.java
+++ src/test/java/umm3601/user/GetUserByIDFromDB.java
@@ -21,20 +21,4 @@ public class GetUserByIDFromDB {
     User user = db.getUser("588935f5556f992bf8f37c01");
     assertEquals("Incorrect name", "Bolton Monroe", user.name);
   }
-
-  /*
-  @Test
-  public void getStokesClaytonFromDB() throws IOException {
-    UserController userController = new UserController("src/main/data/users.json");
-    User user = userController.getUser("588935f52787254123f71fed");
-    assertEquals("Incorrect name", "Stokes Clayton", user.name);
-  }
-
-  @Test
-  public void getBoltonMonroeFromDB() throws IOException {
-    UserController userController = new UserController("src/main/data/users.json");
-    User user = userController.getUser("588935f5556f992bf8f37c01");
-    assertEquals("Incorrect name", "Bolton Monroe", user.name);
-  }
-  */
 }
