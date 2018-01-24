e6203947491436b3fe510f5a24547c7d1b841799
Nic McPhee <mcphee@morris.umn.edu>
2018-01-24 12:49:14 -0600

    Remove all reference to JS tests
    The JS tests didn't really do anything, and we say we don't do any JS
    testing in the `README`, so I've removed any reference to JS tests in
    the code as well.
    
    Closes #18

--- src/test/javascript/clientJavascript.spec.js
+++
@@ -1,9 +0,0 @@
-// Just some example testing.
-
-describe('example testing', function(){
-    it('should return the correct string length', function(){
-        expect("kittens".length).toEqual(7);
-    });
-
-});
-
