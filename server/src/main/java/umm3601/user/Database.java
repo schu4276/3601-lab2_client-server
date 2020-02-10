65b9da7aa02f42dcbe4bc4e5ae63759975ae043f
Waller Li <lsw626313@gmail.com>
2020-02-10 12:01:55 -0600

    Remove a unused import

--- server/src/main/java/umm3601/user/Database.java
+++ server/src/main/java/umm3601/user/Database.java
@@ -5,7 +5,6 @@ import com.google.gson.Gson;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
